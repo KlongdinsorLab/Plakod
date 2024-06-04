005bd2b7b6a12383332ba746c24ccc141e8799e3
EgapNimda <6434449023@student.chula.ac.th>
2024-06-04 16:23:49 +0700

    Empty Heart

--- space_invader/src/scene/LifeCountScene.ts
+++ space_invader/src/scene/LifeCountScene.ts
@@ -2,6 +2,9 @@ import Phaser from "phaser";
 import WebFont from 'webfontloader'
 
 export default class LifeCountScene extends Phaser.Scene{
+    private allheart = 5
+    private showingheart = 1
+
     constructor() {
         super('life_count')
     }
@@ -15,20 +18,33 @@ export default class LifeCountScene extends Phaser.Scene{
 
     create(){
         const { width,height } = this.scale
-
         this.add.tileSprite(0,0,width,height,'bg').setOrigin(0).setScrollFactor(0,0)
 
+        // Black Window
         this.add.rectangle(0, 0, width, height, 0x000000, 0.5).setOrigin(0,0)
 
-        this.add.image(205 + 26, 528, 'sheet', 'heart_full.png').setOrigin(0,0)
+        // Heart 1
+        if (this.showingheart >= 1) { 
+          this.add.image(205 + 26, 528, 'sheet', 'heart_full.png').setOrigin(0,0) 
+        }
+        else {
+          this.add.image(205 + 26, 528, 'sheet', 'heart_empty.png').setOrigin(0,0) 
+        }
 
-        this.add.image(205 + 2+ + 258, 528, 'sheet', 'heart_full.png').setOrigin(1,0)
+        // Heart 2
+        if (this.showingheart >= 2) { 
+          this.add.image(205 + 2+ + 258, 528, 'sheet', 'heart_full.png').setOrigin(1,0)
+        }
+        else {
+          this.add.image(205 + 2+ + 258, 528, 'sheet', 'heart_empty.png').setOrigin(1,0)
+        }
 
-        const HeartText = this.add.text(width/2, 528 + 166 - 20, "หัวใจเหลือ 9 ดวง")
+        const HeartText = this.add.text(width/2, 528 + 166 - 20, `หัวใจเหลือ ${this.allheart} ดวง`)
             .setColor("#DD2E05")
             .setStroke("#FFFFFF",6)
             .setFontSize(32)
             .setPadding(0,20,0,10)
+            .setAlign("center")
             .setOrigin(0.5,0)
 
         WebFont.load({
