84732282c79ac17c4ddbbd47606f4e0b511dbf2c
EgapNimda <6434449023@student.chula.ac.th>
2024-07-08 16:12:38 +0700

    Fix Random Method

--- space_invader/src/component/equipment/weapon/SingleLaser.ts
+++ space_invader/src/component/equipment/weapon/SingleLaser.ts
@@ -1,6 +1,7 @@
 import { Laser } from './Laser'
 import Player from 'component/player/Player'
 import { LASER_SPEED } from 'config'
+import { angularVelocity } from './AngularVelocity'
 // import SoundManager from 'component/sound/SoundManager'
 
 export class SingleLaser extends Laser {
@@ -10,7 +11,7 @@ export class SingleLaser extends Laser {
 	// private shootSound?: Phaser.Sound.BaseSound
 	private soundEffect!: Phaser.Sound.NoAudioSound | Phaser.Sound.WebAudioSound | Phaser.Sound.HTML5AudioSound
 
-	private angularVelocity = [LASER_SPEED / 1, LASER_SPEED / 2, LASER_SPEED / 3, LASER_SPEED / 4, LASER_SPEED / 5, LASER_SPEED / 6, LASER_SPEED / 7, LASER_SPEED / 8]
+
 	private angularVelocityIndex = 1
 
 	constructor(scene: Phaser.Scene, player: Player) {
@@ -20,16 +21,16 @@ export class SingleLaser extends Laser {
 		// this.shootSound = this.scene.sound.add('shootSound', { volume: 2 })
 		this.soundEffect = scene.sound.addAudioSprite('mcSound')
 
-		this.shuffle(this.angularVelocity)
+		this.angularVelocityIndex = Math.floor(Math.random() * 8)
 	}
 	shoot(): Phaser.Types.Physics.Arcade.ImageWithDynamicBody[] {
 		const { x, y } = this.player.getLaserLocation()
 		this.laser = this.scene.physics.add.image(x, y, 'laser')
 		this.laser.setVelocityY(-1 * LASER_SPEED)
-		this.laser.setAngularVelocity(this.angularVelocity[this.angularVelocityIndex])
+		this.laser.setAngularVelocity(angularVelocity[this.angularVelocityIndex])
 		// new SoundManager(this.scene).play(this.shootSound!)
 		this.soundEffect.play("shooting")
-		this.angularVelocityIndex = this.angularVelocityIndex+1%8
+
 		return [this.laser]
 	}
 
@@ -37,18 +38,4 @@ export class SingleLaser extends Laser {
 		this.laser?.destroy()
 	}
 
-	shuffle(array: number[]): void {
-		let currentIndex = array.length,
-			randomIndex
-
-		while (currentIndex != 0) {
-			randomIndex = Math.floor(Math.random() * currentIndex)
-			currentIndex--
-
-			;[array[currentIndex], array[randomIndex]] = [
-				array[randomIndex],
-				array[currentIndex],
-			]
-		}
-	}
 }
