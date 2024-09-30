8271bd7ee0da76fe9815633542620877db24e7de
Boom <pongsabutra@atato.com>
2024-09-30 12:03:14 +0700

    update controller buttons

--- space_invader/src/scene/GameScene.ts
+++ space_invader/src/scene/GameScene.ts
@@ -182,10 +182,16 @@ export default class GameScene extends Phaser.Scene {
 			.defineKey(0, 'B16', 'SPACE')
 			//            .defineKey(0, 'B1', 'CTRL')
 			//            .defineKey(0, 'B2', 'ALT')
-			.defineKey(0, 'B5', 'ONE')
-			.defineKey(0, 'B7', 'TWO')
-			.defineKey(0, 'B4', 'THREE')
-			.defineKey(0, 'B6', 'FOUR')
+			.defineKey(0, 'B6', 'ONE')
+			.defineKey(0, 'B4', 'TWO')
+			.defineKey(0, 'B7', 'THREE')
+			.defineKey(0, 'B5', 'FOUR')
+
+			// .defineKey(0, 'B5', 'ONE')
+			// .defineKey(0, 'B7', 'TWO')
+			// .defineKey(0, 'B4', 'THREE')
+			// .defineKey(0, 'B6', 'FOUR')
+			//
 
 		this.gameLayer = this.add.layer()
 		this.player = new Player(this, this.gameLayer)
@@ -319,7 +325,7 @@ export default class GameScene extends Phaser.Scene {
 		this.tutorial.getStep() > Step.CONTROLLER || this.isCompleteTutorial()
 
 	update(_: number, delta: number) {
-		
+
 		//        if (this.input.gamepad.total === 0) {
 		//            const text = this.add.text(0, height / 2, START_TEXT, {fontSize: '24px'}).setOrigin(0);
 		//            text.x = width / 2 - text.width / 2
@@ -390,13 +396,13 @@ export default class GameScene extends Phaser.Scene {
 			this.controller1?.buttons.B16 > 0
 		) {
 			gauge.hold(delta)
-		} else if (this.controller1?.buttons.B7 > 0) {
-			gauge.setStep(1)
-		} else if (this.controller1?.buttons.B5 > 0) {
-			gauge.setStep(0)
 		} else if (this.controller1?.buttons.B4 > 0) {
-			gauge.setStep(2)
+			gauge.setStep(1)
 		} else if (this.controller1?.buttons.B6 > 0) {
+			gauge.setStep(0)
+		} else if (this.controller1?.buttons.B7 > 0) {
+			gauge.setStep(2)
+		} else if (this.controller1?.buttons.B5 > 0) {
 			gauge.setStep(3)
 		} else {
 			gauge.setVisible(false)
