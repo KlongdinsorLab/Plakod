e41ccc480f31d300e299b59c89c0dee23a7b5cd8
EgapNimda <6434449023@student.chula.ac.th>
2024-11-27 10:04:04 +0700

    integrate sentry

--- space_invader/src/services/API/backend/supabaseClient.ts
+++ space_invader/src/services/API/backend/supabaseClient.ts
@@ -1,6 +1,19 @@
+import * as Sentry from '@sentry/browser'
 import { createClient } from "@supabase/supabase-js";
+import { supabaseIntegration } from '@supabase/sentry-js-integration'
 
 export const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL,import.meta.env.VITE_SUPABASE_ANON_KEY)
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      supabaseIntegration(supabaseClient, Sentry, {
+        tracing: true,
+        breadcrumbs: true,
+        errors: true,
+      }),
+    ],
+  })
 
 //export const testFunction = supabaseClient.functions.invoke('fake-seed',{})
 
