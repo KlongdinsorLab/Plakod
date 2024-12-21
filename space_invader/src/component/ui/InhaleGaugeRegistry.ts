02ff1c158e14e00bc0f4359b74995e014a05d6af
EgapNimda <6434449023@student.chula.ac.th>
2024-12-21 11:34:15 +0700

    add difficulty to inhale gauge

--- space_invader/src/component/ui/InhaleGaugeRegistry.ts
+++ space_invader/src/component/ui/InhaleGaugeRegistry.ts
@@ -8,15 +8,17 @@ import OverlapInhaleGauge from './OverlapInhaleGauge'
 import InhaleGauge from './InhaleGauge'
 export default class InhaleGaugeRegistry {
 	private scene: Phaser.Scene
+	private holdDurationMs: number
 	private holdbars!: Phaser.GameObjects.GameObject[] | any[]
 
-	constructor(scene: Phaser.Scene) {
+	constructor(scene: Phaser.Scene, holdDurationMs: number) {
 		this.scene = scene
+		this.holdDurationMs = holdDurationMs
 	}
 
 	createbyDivision(division: number) {
 		this.holdbars = [...Array(division)].map(
-			(_, index: number) => new OverlapInhaleGauge(this.scene, division, index),
+			(_, index: number) => new OverlapInhaleGauge(this.scene, division, index, this.holdDurationMs),
 		)
 	}
 
